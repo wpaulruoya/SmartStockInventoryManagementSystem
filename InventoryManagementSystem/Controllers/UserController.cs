624d99ab44b97fee68147d9713ac203e83a4112b
pruoya@gmail.com <pruoya@gmail.com>
2025-01-28 15:51:43 +0300

    finished on the UI, will proceed to the db

---
+++ InventoryManagementSystem/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagementSystem.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: /User/Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // GET: /User/Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
