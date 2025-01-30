ee79e1ccf6e63a2f3b838f2dbcae8bfc162882fe
pruoya@gmail.com <pruoya@gmail.com>
2025-01-30 13:07:33 +0300

    Succcessful login

---
+++ InventoryManagementSystem/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using InventoryManagementSystem.Models;
+using System.Linq;
+
+namespace InventoryManagementSystem.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public LoginController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Authenticate(User user)
+        {
+            var existingUser = _context.Users.FirstOrDefault(u => u.Email == user.Email && u.PasswordHash == user.PasswordHash);
+            if (existingUser != null)
+            {
+                return RedirectToAction("Home", "Home");
+            }
+
+            ViewBag.Error = "Invalid email or password.";
+            return View("Index");
+        }
+    }
+}
